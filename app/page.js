8fb572b2ab7f35d637769542c6fba509b327817d
Filiberto Zurita <fmasz6@gmail.com>
2024-03-07 20:54:29 -0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className='text-5xl mb-8 font-bold'>Next.js Tutorial</h1>
+      <Link href='/client' className='btn btn-accent'>
+        get started
+      </Link>
+    </div>
+  );
+}
+
+
